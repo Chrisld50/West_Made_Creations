81b5ea22769519de368304bd6f10102de969aeff
Chrisld50 <chrisld50@yahoo.com>
2023-03-07 22:43:42 -0600

    added to navbar and header, added background image and some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import '../styling/Header.css';
 
 
 export default function Header() {
     return (
 <div>
-    <h2> West Made Creations </h2>
+    <h2 className={'header'}> West Made Creations </h2>
         <Navbar />
 </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Nav, Navbar } from 'react-bootstrap';
+import '../styling/Navbar.css';
 
 export default function NavigationBar() {
     
     return (
         <div>
           <Navbar>
-            <Nav className={"mx-auto"}>
-              <Nav.Link className={""} href="/">Home</Nav.Link>
-              <Nav.Link className={""}  href="/aboutme">About Me</Nav.Link>
+            <Nav className={"mx-auto navbar"}>
+              <Nav.Link className={"navbar"} href="/">Home</Nav.Link>
+              <Nav.Link className={"navbar"}  href="/aboutme">About Me</Nav.Link>
            </Nav>
           <Nav>
           <Nav.Link eventKey={2} href="">
