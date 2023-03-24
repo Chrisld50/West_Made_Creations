0d089da88ce72131c98ffd3a1451f62762e32fb2
Chrisld50 <chrisld50@yahoo.com>
2023-03-23 22:36:03 -0500

    added to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,20 @@ export default function Contact() {
             </div>
 
             <div>
+                <h4>Contact Me</h4>
 
+                <p> If you are interested in a build,<br/> have any questions or difficutlies with your build,<br/> here is how you can get ahold of me. </p>
+
+                <ul>
+                <li><a href="tel:+" target="_blank" rel="noreferrer">111 111 1111</a></li>
+                <li><a href="emailto:" target="_blank" rel="noreferrer"> test@test.com</a></li>
+                <li><a href="linkedin"target="_blank" rel="noreferrer" > Linkedin </a></li>
+                </ul>
+            </div>
+
+            <div> 
+                <h5> Build Info </h5>
+                <p> Each build is made only with premium parts</p>
             </div>
 
             <div>
